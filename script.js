dc484136922edadd5f5be0677816e7cbaa978830
StanovskyDev <stas@vdb.com.pl>
2022-07-19 13:14:47 +0200

    Make menu functional

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 let drawingContainer = document.querySelector(".drawing-container");
 
+let redSlider = document.getElementById("red-slider");
+let greenSlider = document.getElementById("green-slider");
+let blueSlider = document.getElementById("blue-slider");
+let sizeSlider = document.getElementById("size-slider");
+
+let redText = document.querySelector(".slide-container.red > p");
+let greenText = document.querySelector(".slide-container.green > p");
+let blueText = document.querySelector(".slide-container.blue > p");
+let sizeText = document.querySelector(".slide-container.size > p");
+
+let clearButton = document.querySelector(".clear-button");
+let rainbowButton = document.querySelector(".rainbow-button");
+
+let colorDisplay = document.querySelector(".color-display");
+
+console.log(colorDisplay);
+
 let gridSize = 16;
 let color = [0, 0, 0];
+let rainbowMode = false;
+
+function turnOffRainbow(){
+    rainbowMode = false;
+    colorDisplay.classList.remove("rainbow-mode");
+    rainbowButton.classList.remove("button-active");
+}
+
+function clearGrid(){
+    drawingContainer.innerHTML = '';
+}
 
 function createGrid(){
     const qridSizeSquared = gridSize*gridSize;
@@ -9,21 +37,65 @@ function createGrid(){
     drawingContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     for(let i = 0; i < qridSizeSquared; i++){
         let pixel = document.createElement('div');
-        pStyle = pixel.style;
+        pixel.classList.add("pixel");
 
-        pStyle.width = "100%";
-        pStyle.height = "100%";
-        pStyle.backgroundColor = "white"
-        pStyle.boxSizing = "border-box";
-
-        pixel.addEventListener("mouseover", (event) => {pixel.style.opacity = "0.5";})
+        pixel.addEventListener("mouseover", (event) => {
+            pixel.style.opacity = "0.5";
+            console.log(event);
+        })
         pixel.addEventListener("mouseout", (event) => {pixel.style.opacity = "1";})
 
         drawingContainer.appendChild(pixel);
     }
 }
 
+function updateColorDisplay(){
+    colorDisplay.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+}
+
 function setup(){
+    redSlider.oninput = () => {
+        redText.textContent = `Red: ${redSlider.value}`;
+        color[0] = redSlider.value;
+        turnOffRainbow();
+        updateColorDisplay();
+    }
+
+    greenSlider.oninput = () => {
+        greenText.textContent = `Green: ${greenSlider.value}`;
+        color[1] = greenSlider.value;
+        turnOffRainbow();
+        updateColorDisplay();
+    }
+
+    blueSlider.oninput = () => {
+        blueText.textContent = `Blue: ${blueSlider.value}`;
+        color[2] = blueSlider.value;
+        turnOffRainbow();
+        updateColorDisplay();
+    }
+
+    sizeSlider.oninput = () => {
+        sizeText.textContent = `Size: ${sizeSlider.value}x${sizeSlider.value}`;
+        gridSize = sizeSlider.value;
+    }
+    
+    sizeSlider.onchange = () => {
+        clearGrid();
+        createGrid();
+    }
+
+    clearButton.onclick = () => {
+        clearGrid();
+        createGrid();
+    }
+
+    rainbowButton.onclick = () => {
+        rainbowMode = (rainbowMode + 1) % 2;
+        colorDisplay.classList.toggle("rainbow-mode");
+        rainbowButton.classList.toggle("button-active");
+    }
+
     createGrid();
 }
 
